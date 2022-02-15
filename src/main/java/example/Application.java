6815b6ef9a4ae4ddfd8d73f7d321ea289a9a4b3b
Brian Henning <henninb@msn.com>
2022-02-15 06:15:10 -0600

    initial

---
+++ src/main/java/example/Application.java
@@ -0,0 +1,51 @@
+package example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import lombok.RequiredArgsConstructor;
+import example.model.AppUser;
+import example.model.AppUserRole;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import example.service.UserService;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class Application implements CommandLineRunner {
+
+  final UserService userService;
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+
+  @Bean
+  public ModelMapper modelMapper() {
+    return new ModelMapper();
+  }
+
+  @Override
+  public void run(String... params)  {
+    AppUser admin = new AppUser();
+    admin.setUsername("admin");
+    admin.setPassword("admin");
+    admin.setEmail("admin@email.com");
+    admin.setAppUserRoles(new ArrayList<>(Collections.singletonList(AppUserRole.ROLE_ADMIN)));
+
+    userService.signup(admin);
+
+    AppUser client = new AppUser();
+    client.setUsername("client");
+    client.setPassword("client");
+    client.setEmail("client@email.com");
+    client.setAppUserRoles(new ArrayList<>(Collections.singletonList(AppUserRole.ROLE_CLIENT)));
+
+    userService.signup(client);
+  }
+
+}
